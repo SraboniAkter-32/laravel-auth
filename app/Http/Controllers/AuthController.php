1ac3c6bb2320c84e90d5a61f78bf5ea72efaa4eb
Sraboni Akter <sraboniibnataddrija@gmail.com>
2025-08-05 12:39:18 +0600

    auth changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,20 +1,36 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use Validator;
 
 class AuthController extends Controller
 {
     public function __construct(){
-        $this->middleware('auth:api',['except'=>['register','login']]);
+        $this->middleware('auth:api',['except'=>['login','register']]);
     }
 
     public function register(Request $request){
+        $validator=Validator::make($request->all[
+            'name'=>'required',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|string|confirmed|min:6'
+        ])
+        if($validator->fails()){
+            return response()->json($validator->errors()->tojson(),400);
+        }
+        $user=User:create(array_merge(
+            $validator=validated(),
+            ['password'=>bcrypt($request->password)],
+        ));
+        return response()->json([
+            'message'=>'User created successfully',
+        ],201);
         
     }
     public function login(Request $request){
+
 
     }
 }
